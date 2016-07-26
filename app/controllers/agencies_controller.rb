158fcf9d85e633c15eea56738fa7b1198ff998c6
rodk <rod2010rod2010@hotmail.com>
2016-07-26 16:04:27 -0400

    app/controllers/agencies_controller.rb

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -1,4 +1,5 @@
 class AgenciesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
   before_action :set_agency, only: [:show, :edit, :update, :destroy]
 
   # GET /agencies
